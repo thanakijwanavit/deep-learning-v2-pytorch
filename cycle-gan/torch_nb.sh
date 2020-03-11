e8926e88d8bc2fadff4d52e3e31a4c1201e29cc6
Ubuntu <ubuntu@ip-172-31-58-122.ec2.internal>
2020-03-11 16:53:18 +0000

    fix error in residual block codes

--- cycle-gan/torch_nb.sh
+++ cycle-gan/torch_nb.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it -v $(pwd):/home/jovyan/work -v $HOME/scripts/jupyter/.jupyter:/home/jovyan/.jupyter -p 8888:8888 thanakijwanavit/jupyter_cuda:latest
+docker run -it --runtime=nvidia -v $(pwd):/home/jovyan/work -v $HOME/scripts/jupyter/.jupyter:/home/jovyan/.jupyter -p 8888:8888 thanakijwanavit/jupyter_cuda_datascience:latest
