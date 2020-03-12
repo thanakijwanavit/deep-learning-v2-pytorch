772c103f055bed7d64cce923b19a8394a2eeef2d
nic <ubuntu@ip-172-31-58-122.ec2.internal>
2020-03-12 00:17:08 +0000

    add and load sample scripts

---
+++ cycle-gan/samples_cyclegan/load_samples.sh
@@ -0,0 +1,1 @@
+aws s3 cp --recursive s3://udacity-rl-gan/generated-images/samples-cyclegan ../samples_cyclegan 
---
+++ cycle-gan/samples_cyclegan/save_samples.sh
@@ -0,0 +1,1 @@
+aws s3 cp --recursive ../samples_cyclegan s3://udacity-rl-gan/generated-images/samples-cyclegan
